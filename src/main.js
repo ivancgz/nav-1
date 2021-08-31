99e35247d0c56ed588edf6d385ec66647f4d5288
137959887465 <1667884636@qq.com>
2021-08-31 22:11:23 +0800

    finish

--- src/main.js
+++ src/main.js
@@ -3,22 +3,40 @@ const $lastLi = $siteList.find('.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [
-    {logo:'A', logoType: 'text', url:'https://www.acfun.cn'},
-    {logo:'#icon-bilibili', logoType:'image', url:'https://www.bilibili.com'}
+    {logo:'A', url:'https://www.acfun.cn'},
+    {logo:'B', url:'https://www.bilibili.com'}
 
 ]
 
+const simplifyUrl = (url)=>{
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')
+}
+
 const render = () => {
     $siteList.find('li:not(.last)').remove()
-    hashMap.forEach(node=>{
+    hashMap.forEach((node, index)=>{
         const $li = $(`<li>
-        <a href="${node.url}">
-            <div class="site">
-                <div class="logo">${node.logo[0]}</div>
-                <div class="link">${node.url}</div>
+        <div class="site">
+            <div class="logo">${node.logo}</div>
+            <div class="link">${simplifyUrl(node.url)}</div>
+            <div class="close">
+                <svg class="icon">
+                        <use xlink:href="#icon-close"></use>
+                </svg>
             </div>
-        </a>
+        </div>    
     </li>`).insertBefore($lastLi)
+        $li.on('click', ()=>{
+            window.open((node.url))
+        })
+        $li.on('click', '.close', (e)=>{
+            e.stopPropagation()  // 阻止冒泡
+            hashMap.splice(index, 1)
+            render()
+        })
     })
 }
 
@@ -33,7 +51,7 @@ $('.addButton')
         }
         console.log(url)
         hashMap.push({
-            logo: url[0], 
+            logo: simplifyUrl(url)[0].toUpperCase(),
             logoType:'text',
             url: url
         })
@@ -44,6 +62,14 @@ $('.addButton')
 window.onbeforeunload = ()=>{
     const string = JSON.stringify(hashMap)
     localStorage.setItem('x', string)
-
 }
+
+$(document).on('keypress', (e)=>{
+    const {key} = e
+    for(let i=0; i<hashMap.length; i++){
+        if(hashMap[i].logo.toLowerCase() === key){
+            window.open(hashMap[i].url)
+        }
+    }
+})
 
